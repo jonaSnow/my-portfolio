810ad43244836118ed0952a00f2214e6dc1061ac
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2019-04-22 09:28:55 -0400

    added about me page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Card from "./components/Card";
+import Aboutme from "./components/Aboutme";
 import "./styles/styles.scss";
 
 class App extends Component {
@@ -35,7 +36,7 @@ class App extends Component {
             </div>
           </section>
           <section className="mdl-layout__tab-panel" id="fixed-tab-2">
-            <div className="page-content">Jona</div>
+            <Aboutme />
           </section>
         </main>
         <Footer />
